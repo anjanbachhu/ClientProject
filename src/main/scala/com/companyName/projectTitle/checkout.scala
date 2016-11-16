aeb34b86808fbad10a81ad3e6c5515938fb973e4
anjanbachhu <anjan.bachhu@gmail.com>
2016-11-16 23:24:13 +0000

    standard bill implemented

--- src/main/scala/com/companyName/projectTitle/checkout.scala
+++ src/main/scala/com/companyName/projectTitle/checkout.scala
@@ -1,5 +1,5 @@
 package com.companyName.projectTitle
 
 object checkout {
-  def cartAmount(cart: List[Menu]): Double = 0.0
+  def cartAmount(cart: List[Menu]): Double = cart.foldLeft(0.0)((x, Menu) => x + Menu.MenuItemPrice)
 }
